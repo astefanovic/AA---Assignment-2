5f72d94575af9eb7818f909a2edc47a92e5b5058
Drew Nuttall-Smith <drew@mysticpants.com>
2017-05-25 22:22:04 +1000

    Copied unchanged functions from RandomGuessPlayer to MonteCarloGuessPlayer

--- src/player/MonteCarloGuessPlayer.java
+++ src/player/MonteCarloGuessPlayer.java
@@ -1,6 +1,9 @@
 package player;
 
 import java.util.Scanner;
+import java.lang.Math;
+import java.util.ArrayList;
+
 import world.World;
 
 /**
@@ -11,20 +14,66 @@ import world.World;
  */
 public class MonteCarloGuessPlayer  implements Player{
 
+    private World world;
+    private ArrayList<Guess> unmadeGuesses = new ArrayList<Guess>();
+
+    /**
+     * @description perform any initialisation operations to start
+     * @param world this player's world instance
+     * @return void
+     **/
     @Override
     public void initialisePlayer(World world) {
-        // To be implemented.
+        this.world = world;
+
+        // Initialise unmade guesses
+        for (int i = 0; i < world.numRow; i++) {
+            for (int j = 0; j < world.numColumn; j++) {
+                Guess g = new Guess();
+                g.row = i;
+                g.column = j;
+                unmadeGuesses.add(g);
+            }
+        }
     } // end of initialisePlayer()
 
+    /**
+     * @description Determine whether a ship was hit and also if it was sunk.
+     * @param guess The opponent's guess
+     * @return Answer an object stating whether a hit was made and what ship was sunk (if any)
+     **/
     @Override
     public Answer getAnswer(Guess guess) {
-        // To be implemented.
+        Answer answer = new Answer();
 
-        // dummy return
-        return null;
+        // Loop over each ship
+        for (World.ShipLocation sl : world.shipLocations) {
+            // Loop over each coordinate the ship occupies
+            for (World.Coordinate c : sl.coordinates) {
+                // Check if the guess matches the coordinate
+                if (guess.row == c.row && guess.column == c.column) {
+                    answer.isHit = true;
+                    if (isShipSunk(sl))
+                    {
+                        answer.shipSunk = sl.ship;
+                        for(int i = 0; i < world.shipLocations.size(); i++)
+                        {
+                            if(world.shipLocations.get(i).ship.name().equals(answer.shipSunk.name()))
+                                world.shipLocations.remove(i);
+                        }
+                    }
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
     } // end of getAnswer()
 
-
+    /**
+     * @description Make a guess for a location to aim at on opponent's board
+     * @return Guess the location to aim at
+     **/
     @Override
     public Guess makeGuess() {
         // To be implemented.
@@ -39,13 +88,27 @@ public class MonteCarloGuessPlayer  implements Player{
         // To be implemented.
     } // end of update()
 
-
+    /**
+     * @description Check if there are no remaining ships on the board
+     * @return boolean whether there are no remaining ships
+     **/
     @Override
     public boolean noRemainingShips() {
-        // To be implemented.
-
-        // dummy return
-        return true;
+        if(world.shipLocations.isEmpty()) return true;
+        return false;
     } // end of noRemainingShips()
+
+    /**
+     * @description Check if a ship has been sunk
+     * @param World.ShipLocation a ShipLocation object (obtained from the World)
+     * @return boolean whether the ship has been sunk
+     **/
+    private boolean isShipSunk(World.ShipLocation sl) {
+        // Loop over all of the ship's coordinates
+        for (World.Coordinate c : sl.coordinates) {
+            if(!world.shots.contains(c)) return false;
+        }
+        return true;
+    }
 
 } // end of class MonteCarloGuessPlayer
